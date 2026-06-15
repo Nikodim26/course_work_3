863efad21529e5b9f218a2411ee316b9022d4b62
Андрей Васильев <lr1975@yandex.ru>
2026-06-15 16:56:05 +0400

    начало формирования JSON

--- src/main.py
+++ src/main.py
@@ -2,10 +2,12 @@ from day_phase import determining_the_phase_of_the_day
 from views import working_with_transactions
 
 
-def main(request_time: str, period: int = 0) -> None:
-    print(determining_the_phase_of_the_day())
+def main(request_time: str) -> None:
 
-    working_with_transactions(request_time, period)
+    working_with_transactions(request_time)
 
 
-main('2019-05-21 12:49:53')
+
+
+
+print(main('2019-05-21 12:49:53'))
--- src/views.py
+++ src/views.py
@@ -1,15 +1,17 @@
 from datetime import datetime, timedelta
+from typing import Any
 
 import pandas as pd
 
+from day_phase import determining_the_phase_of_the_day
 from reading_the_database import about_financial_transactions_xlsx
 
 
-def working_with_transactions(request_time: str, period: int) -> None:
+def working_with_transactions(request_time: str) -> list[Any]:
     """Функция делает выборки и распечатку транзакций по текущему месяцу относительно заданной даты"""
 
     request_time_end = datetime.strptime(request_time, "%Y-%m-%d %H:%M:%S").date()
-    request_time_start = request_time_end - timedelta(weeks=period * 4, days=request_time_end.day - 1)
+    request_time_start = request_time_end - timedelta(days=request_time_end.day - 1)
 
     financial_transactions = about_financial_transactions_xlsx('operations.xlsx').iloc[:, 1:6]
     financial_transactions['Дата платежа'] = pd.to_datetime(financial_transactions['Дата платежа'])
@@ -23,6 +25,7 @@ def working_with_transactions(request_time: str, period: int) -> None:
     card_numbers = financial_transactions['Номер карты'].unique()
     types_of_currencies = financial_transactions['Валюта операции'].unique()
 
+    cards=[]
     for card_number in card_numbers:
         for currency in types_of_currencies:
             transaction = financial_transactions.loc[
@@ -31,17 +34,33 @@ def working_with_transactions(request_time: str, period: int) -> None:
 
             amount_of_expenses = abs(transaction.loc[transaction['Сумма операции'] < 0, 'Сумма операции'].sum())
 
-            print(f"""
-            За период с {request_time_start} по {request_time_end}
-            
-            Номер карты: {card_number}
-            Общие расходы: {round(amount_of_expenses, 2)} {currency}
-            Kешбэк: {int(amount_of_expenses / 100)} {currency}
-            """)
+            cards.append(
+                {
+                "last_digits": card_number[1:],
+                "total_spent": round(float(amount_of_expenses),2),
+                "cashback": round(float(amount_of_expenses / 100),2)
+                }
+            )
 
-    # return printout
+    json_str = {
+                "greeting": determining_the_phase_of_the_day(),
+                "cards": cards,
+                "top_transactions":
 
-# print((working_with_transactions('2019-05-20 12:49:53')))
+                }
+    #     Номер карты: {card_number}
+    # Общие
+    # расходы: {round(amount_of_expenses, 2)}
+    # {currency}
+    # Kешбэк: {int(amount_of_expenses / 100)}
+    # {currency}
+    # """)
+
+
+    return json_str
+
+
+print(working_with_transactions('2019-05-20 12:49:53'))
 
 ## Запчасти
 # request_time_start = request_time[:8] + '01'
