d4a3f9328938dcff5cf4913a0605a68713bca5d2
Андрей Васильев <lr1975@yandex.ru>
2026-06-16 07:48:51 +0400

    карты и топ в формировании JSON

--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
+import json
+
 from day_phase import determining_the_phase_of_the_day
 from views import working_with_transactions
 
 
 def main(request_time: str) -> None:
-
-    working_with_transactions(request_time)
-
+    s=working_with_transactions(request_time)
+    with open('1.json','w',encoding="utf-8") as f:
+        json.dump(s,f, indent=4, ensure_ascii=False)
 
 
 
--- src/views.py
+++ src/views.py
@@ -1,5 +1,5 @@
+import json
 from datetime import datetime, timedelta
-from typing import Any
 
 import pandas as pd
 
@@ -7,15 +7,17 @@ from day_phase import determining_the_phase_of_the_day
 from reading_the_database import about_financial_transactions_xlsx
 
 
-def working_with_transactions(request_time: str) -> list[Any]:
+def working_with_transactions(request_time: str):
     """Функция делает выборки и распечатку транзакций по текущему месяцу относительно заданной даты"""
+
+    pd.set_option('display.max_columns', None)
 
     request_time_end = datetime.strptime(request_time, "%Y-%m-%d %H:%M:%S").date()
     request_time_start = request_time_end - timedelta(days=request_time_end.day - 1)
 
-    financial_transactions = about_financial_transactions_xlsx('operations.xlsx').iloc[:, 1:6]
-    financial_transactions['Дата платежа'] = pd.to_datetime(financial_transactions['Дата платежа'])
+    financial_transactions = about_financial_transactions_xlsx('operations.xlsx').iloc[:, [1, 2, 3, 4, 5, 9,11]]
 
+    financial_transactions['Дата платежа'] = pd.to_datetime(financial_transactions['Дата платежа'])
     financial_transactions = financial_transactions.loc[
         (financial_transactions['Дата платежа'] <= pd.to_datetime(request_time_end)) &
         (financial_transactions['Дата платежа'] >= pd.to_datetime(request_time_start)) &
@@ -24,6 +26,27 @@ def working_with_transactions(request_time: str) -> list[Any]:
 
     card_numbers = financial_transactions['Номер карты'].unique()
     types_of_currencies = financial_transactions['Валюта операции'].unique()
+    categories = dict(financial_transactions["Категория"].value_counts().nlargest(5))
+
+    top_transactions = []
+    for categori in categories.keys():
+        categori_df = financial_transactions.loc[
+            (financial_transactions["Категория"] == categori)]
+
+        amount_max = categori_df.loc[categori_df['Сумма операции'] < 0, 'Сумма операции'].min()
+        top_transactions_df = categori_df.loc[categori_df['Сумма операции'] == amount_max]
+        date = str(top_transactions_df.iloc[0, 0])
+        date=datetime.strptime(date, "%Y-%m-%d %H:%M:%S").strftime("%d.%m.%Y")
+
+        a = 1
+        top_transactions.append(
+            {
+                "date": date,
+                "amount": abs(round(float(amount_max), 2)),
+                "category": categori,
+                "description": str(top_transactions_df.iloc[0, 6])
+            }
+        )
 
     cards=[]
     for card_number in card_numbers:
@@ -36,29 +59,23 @@ def working_with_transactions(request_time: str) -> list[Any]:
 
             cards.append(
                 {
-                "last_digits": card_number[1:],
-                "total_spent": round(float(amount_of_expenses),2),
-                "cashback": round(float(amount_of_expenses / 100),2)
+                    "last_digits": card_number[1:],
+                    "total_spent": round(float(amount_of_expenses),2),
+                    "cashback": round(float(amount_of_expenses / 100),2)
                 }
             )
 
     json_str = {
                 "greeting": determining_the_phase_of_the_day(),
                 "cards": cards,
-                "top_transactions":
-
+                "top_transactions":top_transactions
                 }
-    #     Номер карты: {card_number}
-    # Общие
-    # расходы: {round(amount_of_expenses, 2)}
-    # {currency}
-    # Kешбэк: {int(amount_of_expenses / 100)}
-    # {currency}
-    # """)
 
 
     return json_str
 
+
+# working_with_transactions('2019-05-20 12:49:53')
 
 print(working_with_transactions('2019-05-20 12:49:53'))
 
