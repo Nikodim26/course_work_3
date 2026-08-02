233394b9837dab904d31588790f8f153710b560a
Андрей Васильев <lr1975@yandex.ru>
2026-08-02 13:02:02 +0400

    подправлена recording_stock_quotes

--- src/views_events.py
+++ src/views_events.py
@@ -41,12 +41,12 @@ def working_with_transactions_events(request_time: str, period: str = "m") -> di
         "expenses": {
             "total_amount": round(total_amount_expenses, 2),
             "main": post_by_category(category_dictionary, total_amount_expenses),
-            'transfers_and_cash': transfer_recording_and_cache(dict(category_dictionary)),
-            'income': record_of_replenishments(category_dictionary_income, total_amount_receipts, df),
-            "currency_rates": recording_exchange_rates(user_currencies, currency_base),
-            "stock_prices": recording_stock_quotes(user_stocks, currency_base)
-        }
+            "transfers_and_cash": transfer_recording_and_cache(dict(category_dictionary))
+        },
+        "income": record_of_replenishments(category_dictionary_income, total_amount_receipts, df),
+        "currency_rates": recording_exchange_rates(user_currencies, currency_base),
+        "stock_prices": recording_stock_quotes(user_stocks, currency_base)
     }
-    logger.info('JSON-запись сформирована')
 
+    logger.info('JSON-запись сформирована')
     return json_str
