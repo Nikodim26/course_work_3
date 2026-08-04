73a81bd0fa0cc45017163ab89ccbf9ab7d8f4909
Андрей Васильев <lr1975@yandex.ru>
2026-08-04 18:35:25 +0400

    ввел datatime в services

--- src/services.py
+++ src/services.py
@@ -16,30 +16,27 @@ def receiving_cashback_data() -> list[dict]:
         return []
 
     df = df.loc[(df['Кэшбэк'].notnull()) & (df['Номер карты'].notnull()) & (df['Статус'].isin(['OK']))]
-    # df['Дата платежа'] = df['Дата платежа'].apply(lambda x: x[-7:])
 
     return df.loc[:, ["Дата платежа", "Кэшбэк", "Категория"]].to_dict(orient='records')
-
-
-transactions = receiving_cashback_data()
 
 
 def determination_of_cashback_categories(transactions: list[dict], year: str, month: str) -> str:
     """Формирует JSON по выгоде категорий кешбэка"""
 
-    dt = datetime(int(year), int(month), 0, 0, 0, 0)
-    dt=datetime.strptime(dt, '%Y-%m-%d')
+    dt_search = datetime.strftime(datetime(int(year), int(month), 1, 0, 0, 0), '%m.%Y')
 
-    categories = set(i['Категория'] for i in transactions if i["Дата платежа"] == f'0{month}.{year}')
+    fn = lambda x: datetime.strftime(datetime.strptime(x, '%d.%m.%Y'), '%m.%Y')
+    b = fn('30.12.2021')
+
+    categories = set(i['Категория'] for i in transactions if fn(i["Дата платежа"]) == dt_search)
 
     cashback_by_category = {}
     for category in categories:
         cashback_list = [float(i['Кэшбэк']) for i in transactions if
-             i["Категория"] == category and i["Дата платежа"] == f'0{month}.{year}']
+                         i["Категория"] == category and fn(i["Дата платежа"]) == dt_search]
 
         cashback_by_category[category] = round(sum(cashback_list))
         cashback_list_sorted = dict(sorted(cashback_by_category.items(), key=lambda x: x[1], reverse=True))
-
 
     return json.dumps(cashback_list_sorted, indent=4, ensure_ascii=False)
 
